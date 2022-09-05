d6c684ce21e9229ee9da629d3f708964ed256f8a
Skonik <s.konik.dev@gmail.com>
2022-09-05 22:23:04 +0300

    fix read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
 
 project = 'docker-compose-diagram'
 copyright = '2022, Sergei Konik'
@@ -18,9 +19,14 @@ templates_path = ['_templates']
 exclude_patterns = []
 extensions = ['myst_parser']
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
