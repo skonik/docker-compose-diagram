22fe859b0da64ad3d7d95160bd61f8431e089daf
Skonik <s.konik.dev@gmail.com>
2023-01-21 20:01:41 +0400

    feature(extend-images): fix regex and bump diagrams version

--- docker_compose_diagram/docker_images/patterns.py
+++ docker_compose_diagram/docker_images/patterns.py
@@ -47,172 +47,172 @@ class NginxImage(DockerImagePattern):
 
 
 class PythonImage(DockerImagePattern):
-    pattern = r"python"
+    pattern = r"^python"
     diagram_render_class = Python
 
 
 class MysqlImage(DockerImagePattern):
-    pattern = r"mysql"
+    pattern = r"^mysql"
     diagram_render_class = MySQL
 
 
 class SNSImage(DockerImagePattern):
-    pattern = r"sns$"
+    pattern = r"^sns$"
     diagram_render_class = SNS
 
 
 class SQSImage(DockerImagePattern):
-    pattern = r"sqs"
+    pattern = r"^sqs"
     diagram_render_class = SQS
 
 
 class DynamoDBImage(DockerImagePattern):
-    pattern = r"dynamodb"
+    pattern = r"^dynamodb"
     diagram_render_class = Dynamodb
 
 
 class RubbyImage(DockerImagePattern):
-    pattern = r"ruby"
+    pattern = r"^ruby"
     diagram_render_class = Ruby
 
 
 class GolangImage(DockerImagePattern):
-    pattern = r"golang"
+    pattern = r"^golang"
     diagram_render_class = Go
 
 
 class RabbitMQImage(DockerImagePattern):
-    pattern = r"rabbitmq"
+    pattern = r"^rabbitmq"
     diagram_render_class = RabbitMQ
 
 
 class DjangoImage(DockerImagePattern):
-    pattern = r"django"
+    pattern = r"^django$"
     diagram_render_class = Django
 
 
 class CeleryImage(DockerImagePattern):
-    pattern = r"celery"
+    pattern = r"^celery$"
     diagram_render_class = Celery
 
 
 class FlaskImage(DockerImagePattern):
-    pattern = r"flask"
+    pattern = r"^flask$"
     diagram_render_class = Flask
 
 
 class S3Image(DockerImagePattern):
-    pattern = r"s3"
+    pattern = r"^s3$"
     diagram_render_class = S3
 
 
 class RailsImage(DockerImagePattern):
-    pattern = r"rails"
+    pattern = r"^rails"
     diagram_render_class = Rails
 
 
 class RustImage(DockerImagePattern):
-    pattern = r"rust"
+    pattern = r"^rust"
     diagram_render_class = Rust
 
 
 class KotlinImage(DockerImagePattern):
-    pattern = r"kotlin"
+    pattern = r"^kotlin"
     diagram_render_class = Kotlin
 
 
 class NodeJSImage(DockerImagePattern):
-    pattern = r"node"
+    pattern = r"^node"
     diagram_render_class = NodeJS
 
 
 class JavaScriptImage(DockerImagePattern):
-    pattern = r"javascript"
+    pattern = r"^javascript"
     diagram_render_class = Javascript
 
 
 class MongodbImage(DockerImagePattern):
-    pattern = r"mongo"
+    pattern = r"^mongo"
     diagram_render_class = Mongodb
 
 
 class ReactImage(DockerImagePattern):
-    pattern = r"react"
+    pattern = r"^react"
     diagram_render_class = React
 
 
 class VueImage(DockerImagePattern):
-    pattern = r"vue"
+    pattern = r"^vue"
     diagram_render_class = Vue
 
 
 class KafkaImage(DockerImagePattern):
-    pattern = r"kafka"
+    pattern = r"^kafka"
     diagram_render_class = Kafka
 
 
 class TraefikImage(DockerImagePattern):
-    pattern = r"traefik"
+    pattern = r"^traefik"
     diagram_render_class = Traefik
 
 
 class CaddyImage(DockerImagePattern):
-    pattern = r"caddy"
+    pattern = r"^caddy"
     diagram_render_class = Caddy
 
 
 class FastAPIImage(DockerImagePattern):
-    pattern = r"fastapi"
+    pattern = r"^fastapi"
     diagram_render_class = FastAPI
 
 
 class AngularImage(DockerImagePattern):
-    pattern = r"angular"
+    pattern = r"^angular"
     diagram_render_class = Angular
 
 
 class BackboneImage(DockerImagePattern):
-    pattern = r"backbone"
+    pattern = r"^backbone"
     diagram_render_class = Backbone
 
 
 class EmberImage(DockerImagePattern):
-    pattern = r"ember"
+    pattern = r"^ember"
     diagram_render_class = Ember
 
 
 class FlutterImage(DockerImagePattern):
-    pattern = "flutter"
+    pattern = "^flutter"
     diagram_render_class = Flutter
 
 
 class GraphqlImage(DockerImagePattern):
-    pattern = r"graphql"
+    pattern = r"^graphql$"
     diagram_render_class = GraphQL
 
 
 class LaravelImage(DockerImagePattern):
-    pattern = r"laravel"
+    pattern = r"^laravel$"
     diagram_render_class = Laravel
 
 
 class MicronautImage(DockerImagePattern):
-    pattern = r"micronaut"
+    pattern = r"^micronaut$"
     diagram_render_class = Micronaut
 
 
 class SpringImage(DockerImagePattern):
-    pattern = r"spring"
+    pattern = r"^spring"
     diagram_render_class = Spring
 
 
 class StarletteImage(DockerImagePattern):
-    pattern = r"starlette"
+    pattern = r"^starlette$"
     diagram_render_class = Starlette
 
 
 class BashImage(DockerImagePattern):
-    pattern = r"bash"
+    pattern = r"^bash$"
     diagram_render_class = Bash
 
 
@@ -222,47 +222,47 @@ class CImage(DockerImagePattern):
 
 
 class CPPImage(DockerImagePattern):
-    pattern = r"cpp"
+    pattern = r"^cpp$"
     diagram_render_class = Cpp
 
 
 class CSharpImage(DockerImagePattern):
-    pattern = r"csharp"
+    pattern = r"^csharp$"
     diagram_render_class = Csharp
 
 
 class DartImage(DockerImagePattern):
-    pattern = r"dart"
+    pattern = r"^dart"
     diagram_render_class = Dart
 
 
 class ElixirImage(DockerImagePattern):
-    pattern = r"elixir"
+    pattern = r"^elixir"
     diagram_render_class = Elixir
 
 
 class ErlangImage(DockerImagePattern):
-    pattern = r"erlang"
+    pattern = r"^erlang"
     diagram_render_class = Erlang
 
 
 class JavaImage(DockerImagePattern):
-    pattern = r"java"
+    pattern = r"^java"
     diagram_render_class = Java
 
 
 class LatexImage(DockerImagePattern):
-    pattern = r"latex"
+    pattern = r"^latex"
     diagram_render_class = Latex
 
 
 class MatlabImage(DockerImagePattern):
-    pattern = r"matlab"
+    pattern = r"^matlab$"
     diagram_render_class = Matlab
 
 
 class PHPImage(DockerImagePattern):
-    pattern = r"php"
+    pattern = r"^php"
     diagram_render_class = PHP
 
 
@@ -272,32 +272,32 @@ class RImage(DockerImagePattern):
 
 
 class ScalaImage(DockerImagePattern):
-    pattern = r"scala"
+    pattern = r"^scala"
     diagram_render_class = Scala
 
 
 class SwiftImage(DockerImagePattern):
-    pattern = r"swift"
+    pattern = r"^swift"
     diagram_render_class = Swift
 
 
 class TypeScriptImage(DockerImagePattern):
-    pattern = r"typescript"
+    pattern = r"^typescript"
     diagram_render_class = Typescript
 
 
 class DatacenterImage(DockerImagePattern):
-    pattern = r"datacenter"
+    pattern = r"^datacenter$"
     diagram_render_class = Datacenter
 
 
 class StorageImage(DockerImagePattern):
-    pattern = r"storage"
+    pattern = r"^storage$"
     diagram_render_class = Storage
 
 
 class LinuxGeneralImage(DockerImagePattern):
-    pattern = r"linuxgeneral"
+    pattern = r"^linuxgeneral$"
     diagram_render_class = LinuxGeneral
 
 
@@ -312,7 +312,7 @@ class RackImage(DockerImagePattern):
 
 
 class CertificateImage(DockerImagePattern):
-    pattern = r"^certificate"
+    pattern = r"^certificate$"
     diagram_render_class = Certificate
 
 
